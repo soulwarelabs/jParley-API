ed86d4a80c8cf9f1082310e7ae2b527420f7628c
Ilya Gubarev <ilya.gubarev@gmail.com>
2014-04-08 12:21:11 +0400

    Function added

---
+++ src/main/java/com/soulwarelabs/jparley/Function.java
@@ -0,0 +1,34 @@
+/*
+ * Project:  jParley-API
+ * Outline:  jParley framework API components
+ *
+ * File:     Function.java
+ * Folder:   /.../com/soulwarelabs/jparley
+ * Revision: 1.05, 08 April 2014
+ * Created:  08 February 2014
+ * Author:   Ilya Gubarev
+ *
+ * Copyright (c) 2013-2014 Soulware Labs, Ltd.
+ * Contact information is available at http://www.soulwarelabs.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.soulwarelabs.jparley;
+
+/**
+ *
+ * @author gubarev
+ */
+public interface Function extends Subroutine {
+
+}
