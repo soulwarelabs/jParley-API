2534c4f0a3fec9d4ac787225eb5415907cde35e2
Ilya Gubarev <ilya.gubarev@gmail.com>
2014-06-16 20:52:25 +0400

    Function API updated

--- src/main/java/com/soulwarelabs/jparley/Function.java
+++ src/main/java/com/soulwarelabs/jparley/Function.java
@@ -4,7 +4,7 @@
  *
  * File:     Function.java
  * Folder:   /.../com/soulwarelabs/jparley
- * Revision: 1.12, 16 June 2014
+ * Revision: 1.14, 16 June 2014
  * Created:  08 February 2014
  * Author:   Ilya Gubarev
  *
@@ -40,6 +40,28 @@ import com.soulwarelabs.jcommons.Box;
 public interface Function extends Subroutine {
 
     /**
+     * Gets function result SQL data decoder (optional).
+     *
+     * @return SQL data decoder (optional).
+     *
+     * @see Converter
+     *
+     * @since v1.0.0
+     */
+    Converter getDecoder();
+
+    /**
+     * Sets function result SQL data decoder (optional).
+     *
+     * @param decoder SQL data decoder (optional).
+     *
+     * @see Converter
+     *
+     * @since v1.0.0
+     */
+    void setDecoder(Converter decoder);
+
+    /**
      * Gets a boxed result of the function.
      *
      * @return boxed function result.
@@ -49,4 +71,40 @@ public interface Function extends Subroutine {
      * @since v1.0.0
      */
     Box<Object> getResult();
+
+    /**
+     * Gets function result SQL structure name.
+     *
+     * @return SQL structure name (optional).
+     *
+     * @since v1.0.0
+     */
+    String getStruct();
+
+    /**
+     * Sets function result SQL structure name.
+     *
+     * @param struct SQL structure name (optional).
+     *
+     * @since v1.0.0
+     */
+    void setStruct(String struct);
+
+    /**
+     * Gets function result SQL type code.
+     *
+     * @return SQL type code.
+     *
+     * @since v1.0.0
+     */
+    int getType();
+
+    /**
+     * Sets function result SQL type code.
+     *
+     * @param type SQL type code.
+     *
+     * @since v1.0.0
+     */
+    void setType(int type);
 }
